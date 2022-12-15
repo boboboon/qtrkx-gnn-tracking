5aa74611c54fd875a71069a72610cf6264c864a2
Lucas Curtin <xzcaplcu@dias.hpc.phys.ucl.ac.uk>
2022-12-15 16:13:45 +0000

    IDK

---
+++ submitScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#submit to the normal COMPUTE partition for normal CPUS
+#SBATCH -p COMPUTE
+#request a different amount of time to the default 12h
+#SBATCH --time 24:00:00
+#requesting one node
+#SBATCH -N1
+#requesting 12 cpus
+#SBATCH -n12
+#SBATCH --mail-user=zcaplcu@ucl.ac.uk
+#SBATCH --mail-type=ALL
+eval "$(/share/apps/anaconda/3-2022.05/bin/conda shell.bash hook)"
+conda activate env
+cd /home/xzcaplcu/repo/qtrkx-gnn-tracking/
+srun python3 train.py configs/CGNN_iter1.yaml 20 
