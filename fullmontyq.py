f2e07ec4dc4a7d681a89431a833d49273d0692cc
Lucas <lucas.curtin@gmail.com>
2023-01-22 23:25:09 +0000

    Data Upload

---
+++ fullmontyq.py
@@ -0,0 +1,48 @@
+import sys
+import os
+# Turn off warnings and errors due to TF libraries
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  
+import time
+import datetime
+import csv
+from random import shuffle
+import tensorflow as tf
+# import internal scripts
+from tools.tools import *
+from test import test
+import train_script
+
+
+
+
+
+
+
+# So we want a script that trains a CGNN 5 times (updating the RID each time), then changes the number of iterations (increase from 2, 3 times i.e.: 1,3,5,7)
+
+RID_input=['1','2','3','4','5']
+
+
+config_input_list=[r'configs/CGNN_iter1.yaml',r'configs/CGNN_iter3.yaml',r'configs/CGNN_iter5.yaml',r'configs/CGNN_iter7.yaml']
+
+train_script.main('configs/remote_CGNN.yaml','4')
+
+
+#for i in range(1):
+    #config=config_input_list[i]
+    #iter=2*i+1
+    #for v in range(1):
+       # RID='{}-{}'.format(iter,v)
+       # print(config)
+       # print(RID)
+       # main(config,RID)
+
+
+   
+
+
+
+
+
+#for i in whatever:
+    #main(config[i], whatever)
---
+++ submitScriptQ.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#submit to the normal COMPUTE partition for normal CPUS
+#SBATCH -p COMPUTE
+#request a different amount of time to the default 12h
+#SBATCH --time 24:00:00
+#requesting one node
+#SBATCH -N1
+#requesting 12 cpus
+#SBATCH -n1
+#SBATCH --mail-user=zcaplcu@ucl.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH -o /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/output.out
+#SBATCH -e /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/error.out
+#SBATCH --mem-per-cpu 4G 
+eval "$(/share/apps/anaconda/3-2022.05/bin/conda shell.bash hook)"
+conda activate qtrkx
+cd /home/xzcaplcu/repo/qtrkx-gnn-tracking/
+srun python3 train_quantum.py configs/remote_QGNN.yaml 1
