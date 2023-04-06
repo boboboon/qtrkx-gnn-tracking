bb364715b105eaf874ed2df98b4d0588500bb998
Lucas <lucas.curtin@gmail.com>
2023-04-06 20:50:07 +0100

    Messed up measurements

--- submitScript.sh
+++ submitScript.sh
@@ -9,11 +9,11 @@
 #SBATCH -n1
 #SBATCH --mail-user=zcaplcu@ucl.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH -o /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/output.out
-#SBATCH -e /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/error.out
+#SBATCH -o /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/10output.out
+#SBATCH -e /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/10error.out
 #SBATCH --mem-per-cpu 20G 
 eval "$(/share/apps/anaconda/3-2022.05/bin/conda shell.bash hook)"
 conda activate qtrkx
 cd /home/xzcaplcu/repo/qtrkx-gnn-tracking/
-srun python3 train.py configs/remote_QGNN.yaml thesis2
+srun python3 train.py configs/remote_MPS.yaml MPS-3
 
