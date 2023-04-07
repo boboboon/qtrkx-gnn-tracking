0d9f2e29d943b9e579b39fdf72aac66e16353107
Lucas <lucas.curtin@gmail.com>
2023-04-07 18:32:51 +0100

    SLURM UPDATES

--- submitScript.sh
+++ submitScript.sh
@@ -9,11 +9,11 @@
 #SBATCH -n1
 #SBATCH --mail-user=zcaplcu@ucl.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH -o /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/10output.out
-#SBATCH -e /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/10error.out
+#SBATCH -o /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/MPSoutput.out
+#SBATCH -e /home/xzcaplcu/repo/qtrkx-gnn-tracking/slurms/MPSerror.out
 #SBATCH --mem-per-cpu 20G 
 eval "$(/share/apps/anaconda/3-2022.05/bin/conda shell.bash hook)"
 conda activate qtrkx
 cd /home/xzcaplcu/repo/qtrkx-gnn-tracking/
-srun python3 train.py configs/remote_MPS.yaml MPS-3
+srun python3 train.py configs/remote_MPS.yaml MPS-4
 
